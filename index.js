c9294ca6a4897f39adb713019d33d3045398ec2a
lexis-webb <lexiswebb.design@gmail.com>
2024-12-19 10:38:55 -0500

    express server running

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5050
+
+
+
+app.listen(port, () =>{
+    console.log(`listening on port: ${process.env.PORT}`)
+})
